dcbb64b38b38c2e174e67d3e11e932b77d1aa8d4
Gabriel Gonçalves <gabriel.ggoncalves@linx.com.br>
2022-07-24 20:53:38 -0300

    Pratica export

---
+++ HTML_CSS3_JS/JavaScript/Modulo/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Gabriel', 25));
+console.log(mostraIdade('Gabriel', 'Joinville'));
+console.log(mostraIdade('Gabriel', 'musica'));
